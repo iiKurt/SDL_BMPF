5918e247cd97aaf5d13b44a1c6965e7fb96acf06
iiKurt <38019652+iiKurt@users.noreply.github.com>
2023-01-01 14:00:33 +1300

    Fix some typos

--- BMPF.h
+++ BMPF.h
@@ -1,7 +1,9 @@
 #include <SDL2/SDL.h>
 
+#include <stdbool.h>
+
 void BMPF_initalise(SDL_Texture* font);
 void BMPF_kill();
-void BMPF_setColorFromSDL(SDL_Color* color);
+void BMPF_setColorFromSDL(SDL_Renderer* renderer);
 void BMPF_setColor(Uint32 color, Uint32 unused); /* Color must be in 0x00RRGGBB format! */
-void BMPF_print(SDL_Renderer* dst, const char* str, Uint32 x, Uint32 y, bool alignCenter = false);
+void BMPF_print(SDL_Renderer* dst, const char* str, Uint32 x, Uint32 y, bool alignCenter);
